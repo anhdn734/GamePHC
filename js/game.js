6eba7a6d30c4fe3b806e991aa70d496eef2abad0
Anh Doan <anhdoan621@gmail.com>
2016-11-15 22:54:11 +0700

    forgot to revert this file

--- js/game.js
+++ js/game.js
@@ -5,19 +5,19 @@ RPGJS.defines({
 
 	RPGJS.Player.init({
 		actor: 1,
-		//start: {x: 1, y: 1, id: 1} // GAME_START
-        start: {x: 25, y: 10, id: 13}
+		start: {x: 1, y: 1, id: 1} // GAME_START
+        // start: {x: 25, y: 10, id: 13}
 	});
 
 	RPGJS.Scene.map(function() {
 
         /* game begin intro */
         var common = Class.New("Game_CommonEvents", ["MAP_1_SHOWINTRO"]);
-        //common.exec();
+        common.exec();
 
         /* I need to set some variable here */
         // pass MAP-1
-        RPGJS.Switches.set("MAP_1_NGUYENTRAI", true);
+        /*RPGJS.Switches.set("MAP_1_NGUYENTRAI", true);
         RPGJS.Switches.set("MAP_1_THODIA", true);
 
         RPGJS.Switches.set("MAP_2_CONIEMTIN", true);
@@ -29,7 +29,7 @@ RPGJS.defines({
         RPGJS.Switches.set("STAGE_2_FINISH", true);
         RPGJS.Switches.set("MAP_6_CHO_BINH_THU", true);
         RPGJS.Switches.set("STAGE_3_FINISH", true);
-        RPGJS.Switches.set("MAP_7_DALAYDUOCBONGHOA", true);
+        RPGJS.Switches.set("MAP_7_DALAYDUOCBONGHOA", true);*/
 
         /* to play a sound */
         // RPGJS.System.bgmPlay(1);
