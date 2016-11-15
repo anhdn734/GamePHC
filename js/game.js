b486d0b9474791579064ad728134d10af2e3a16e
Anh Doan <anhdoan621@gmail.com>
2016-11-15 21:25:14 +0700

    developement v3

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ RPGJS.defines({
 	RPGJS.Player.init({
 		actor: 1,
 		start: {x: 1, y: 1, id: 1} // GAME_START
-                // start: {x: 5, y: 10, id: 8}
+        // start: {x: 5, y: 10, id: 11}
 	});
 
 	RPGJS.Scene.map(function() {
@@ -17,17 +17,18 @@ RPGJS.defines({
 
         /* I need to set some variable here */
         // pass MAP-1
-        //RPGJS.Switches.set("MAP_1_NGUYENTRAI", true);
-        //RPGJS.Switches.set("MAP_1_THODIA", true);
+        /*RPGJS.Switches.set("MAP_1_NGUYENTRAI", true);
+        RPGJS.Switches.set("MAP_1_THODIA", true);
 
-        //RPGJS.Switches.set("MAP_2_CONIEMTIN", true);
-        //RPGJS.Switches.set("MAP_3_MUALUONGTHUC", true);
-        //RPGJS.Switches.set("MAP_4_MOSAT", true);
-        //RPGJS.Switches.set("MAP_2_DADAUQUAN", true);
-        //RPGJS.Switches.set("STAGE_1_FINISH", true);
-        //RPGJS.Switches.set("MAP_13_CHUTHANH", true);
-        //RPGJS.Switches.set("STAGE_2_FINISH", true);
-        //RPGJS.Switches.set("MAP_6_CHO_BINH_THU", true);
+        RPGJS.Switches.set("MAP_2_CONIEMTIN", true);
+        RPGJS.Switches.set("MAP_3_MUALUONGTHUC", true);
+        RPGJS.Switches.set("MAP_4_MOSAT", true);
+        RPGJS.Switches.set("MAP_2_DADAUQUAN", true);
+        RPGJS.Switches.set("STAGE_1_FINISH", true);
+        RPGJS.Switches.set("MAP_13_CHUTHANH", true);
+        RPGJS.Switches.set("STAGE_2_FINISH", true);
+        RPGJS.Switches.set("MAP_6_CHO_BINH_THU", true);
+        RPGJS.Switches.set("STAGE_3_FINISH", true);*/
 
         /* to play a sound */
         // RPGJS.System.bgmPlay(1);
@@ -38,6 +39,8 @@ function wait5s() {
     console.log('Waiting...')
     setTimeout(function() {
         console.log('SET')
-        RPGJS.Switches.set("MAP_6_TINTUC", true);
+        // RPGJS.Switches.set("MAP_6_TINTUC", true);
+        var common = Class.New("Game_CommonEvents", ["MAP_6_CUOCCHIEN1"]);
+        common.exec();
     }, 2000);
 }
